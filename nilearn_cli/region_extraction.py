d0eb6ad5a3808abe717f922899d1d8f48140b7ae
Daniel Gomez <d.gomez@donders.ru.nl>
2018-04-20 15:42:10 +0200

    PEP8

--- nilearn_cli/region_extraction.py
+++ nilearn_cli/region_extraction.py
@@ -11,24 +11,29 @@ from nilearn.regions import connected_regions
 from nilearn import plotting
 import matplotlib.pyplot as plt
 
+
 def threshold_map(inputfile, threshold):
     threshold_percentile_img = threshold_img(inputfile, threshold=threshold)
     return threshold_percentile_img
 
+
 def extract_region(threshold_percentile_img, min_region_size):
-    regions_percentile_img, index = connected_regions(threshold_percentile_img,
-                                                  min_region_size=min_region_size)
-    return regions_percentile_img
+    regions_img, index = connected_regions(threshold_percentile_img,
+                                           min_region_size=min_region_size)
+    return regions_img
+
 
 def savefig_regions(regions_percentile_img, inputfile, outputfile):
     fig = plt.figure()
     plotting.plot_prob_atlas(regions_percentile_img, bg_img=inputfile,
-                        view_type='contours', display_mode='z',
-                        cut_coords=10, title='regions', figure=fig)
+                             view_type='contours', display_mode='z',
+                             cut_coords=10, title='regions', figure=fig)
     plt.savefig(outputfile + '_region_extraction.png')
 
-def save_regions(regions_percentile_img, outputfile):
-    regions_percentile_img.to_filename(outputfile + '_region_extraction.nii.gz')
+
+def save_regions(regions_img, outputfile):
+    regions_img.to_filename(outputfile + '_region_extraction.nii.gz')
+
 
 def _cli_parser():
 
@@ -48,7 +53,7 @@ def _cli_parser():
 
     parser.add_argument('--threshold', type=threshold_type, default='97',
                         help='Threshold percentile: 0 < x < 100. Ex. 98')
-    
+
     parser.add_argument('--min_region_size', type=int, default=1500,
                         help='Minimal region size in voxels. Default is 1500')
 
@@ -59,17 +64,20 @@ def main(args):
     # -- Check inputs --
     assert op.exists(args.infile), 'Input file not found.'
     assert op.exists(args.outfile), 'Output file not defined.'
-    
+
     # -- Compute and save the regions --
     threshold_percentile_img = threshold_map(args.infile, args.threshold)
-    regions_percentile_img = extract_region(threshold_percentile_img, args.min_region_size)
+    regions_percentile_img = extract_region(threshold_percentile_img,
+                                            args.min_region_size)
     savefig_regions(regions_percentile_img, args.infile, args.outfile)
     save_regions(regions_percentile_img, args.outfile)
+
 
 def run_region_extraction():
     parser = _cli_parser()
     args = parser.parse_args()
     main(args)
+
 
 if __name__ == '__main__':
     run_region_extraction()
